e6ee101bbcbd2de9107484a53022bc3604e69226
Robert Sirota <rsirota@buffalo.edu>
2020-12-04 14:52:11 -0500

    Comments Added

--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -11,8 +11,8 @@ export const initialState = {
     switch (action.type) {
       case "ADD_TO_BASKET":
         return {
-          ...state,
-          basket: [...state.basket, action.item],
+          ...state, //The basket before
+          basket: [...state.basket, action.item], //Adds item to basket
         };
       
       case 'EMPTY_BASKET':
@@ -22,21 +22,24 @@ export const initialState = {
         }
   
       case "REMOVE_FROM_BASKET":
+        //Going through the basket items to see if any of the ids matches
+        //Finds first match
         const index = state.basket.findIndex(
           (basketItem) => basketItem.id === action.id
         );
+        //Creates temp basket to work with
         let newBasket = [...state.basket];
+        
+        if (index >= 0) { //If found an item
+          newBasket.splice(index, 1); //Removes the item
   
-        if (index >= 0) {
-          newBasket.splice(index, 1);
-  
-        } else {
+        } else { //If didn't find an item
           console.warn(
             `Product (id: ${action.id}) is not in basket`
           )
         }
   
-        return {
+        return { //Lets the new basket be the old basket
           ...state,
           basket: newBasket
         }
