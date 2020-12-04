26f5f4109c146bbd2c18d93c73fe4f6cbd2cbb32
Robert Sirota <rsirota@buffalo.edu>
2020-12-04 14:54:10 -0500

    Added Basic Recently Viewed Items, Needs Work

---
+++ amazon-clone/src/RecentViewItems.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RecentViewItems.css'
+
+function RecentViewItems() {
+    return (
+        <div className="recentviewitems">
+            <p className="recentviewitems">Recently Viewed Items</p>
+        </div>
+    )
+}
+
+export default RecentViewItems
