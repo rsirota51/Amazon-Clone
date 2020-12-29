6d9a7eb96759e7ab94754aab3fd2c45c5b469fc3
Robert Sirota <rsirota@buffalo.edu>
2020-12-29 12:37:53 -0500

    Added Firebase

---
+++ amazon-clone/src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyByVsQg2BAYW3htPeIxHwjTHvg5XJOV1BU",
+    authDomain: "clone-ee51e.firebaseapp.com",
+    projectId: "clone-ee51e",
+    storageBucket: "clone-ee51e.appspot.com",
+    messagingSenderId: "196380437017",
+    appId: "1:196380437017:web:e3f778b5ae2601cf9c9888",
+    measurementId: "G-BER4E8QYXP"
+  };
